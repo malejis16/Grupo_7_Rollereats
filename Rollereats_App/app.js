d7614ca0ac394e37dd0594b0554d152e01cdc51b
Alejandra Nieto <malejis16@hotmail.com>
2022-02-25 16:17:40 -0500

    actualizacion ingreso un solo form

--- Rollereats_App/app.js
+++ Rollereats_App/app.js
@@ -2,24 +2,28 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.listen(3001  , ()=>{
-    console.log('Server Runing');
+app.listen(3001, () => {
+  console.log("Server Runing");
 });
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
-app.get('/Ingresar', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+app.get("/Ingresar", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
 });
-app.get('/Registro', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+app.get("/Registro", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
 });
-app.get('/Carrito', (req,res)=>{
-    res.sendFile(__dirname + '/views/productCart.html');
+app.get("/Carrito", (req, res) => {
+  res.sendFile(__dirname + "/views/productCart.html");
 });
-app.get('/productoDetalle', (req,res)=>{
-    res.sendFile(__dirname + '/views/productDetail.html');
+app.get("/productoDetalle", (req, res) => {
+  res.sendFile(__dirname + "/views/productDetail.html");
+});
+
+app.post("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
