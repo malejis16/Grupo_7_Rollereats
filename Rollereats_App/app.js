3a6d9e006972befdef2b0cf0adb69fd84c589ced
Alejandra Nieto <malejis16@hotmail.com>
2022-03-06 20:02:48 -0500

    Actualizacion Carpetas versión EJS + Express - Nuevo Ruteo

--- Rollereats_App/app.js
+++ Rollereats_App/app.js
@@ -2,17 +2,34 @@
 const express = require("express");
 const app = express();
 var path = require("path");
-const rutas = require("./src/routes/mainRouter");
+const rutas = require("./routes/mainRouter");
 
 app.use(express.static("public"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//Rutas Inicio
 app.use("/", rutas);
+
+//Rutas Users
 app.use("/login", rutas);
-app.use("/productCart", rutas);
-app.use("/productDetail", rutas);
 app.use("/register", rutas);
+
+//Rutas Productos
+app.use("/carro", rutas);
+
+//Rutas Restaurantes
+app.use("/restaurante", rutas);
+app.use("/registro_Restaurante", rutas);
+app.use("/registro_Comercio", rutas);
+
+//Rutas Repartidor
+app.use("/repartidor", rutas);
+app.use("/registro_Repartidor", rutas);
+app.use("/login_Repartidor", rutas);
+
+//Rutas Nosotros
+app.use("/about", rutas);
 
 app.listen(3000, () => {
   console.log("Servidor arriba en el puerto 3000 🤓👌");
---
+++ Rollereats_App/controllers/mainController.js
@@ -0,0 +1,40 @@
+//Módulos
+const path = require("path");
+
+const mainController = {
+  index: function (req, res) {
+    res.render("index");
+  },
+  login: function (req, res) {
+    res.render("users/login");
+  },
+  register: function (req, res) {
+    res.render("users/register");
+  },
+  carro: function (req, res) {
+    res.render("productos/carro");
+  },
+  restaurante: function (req, res) {
+    res.render("restaurantes/restaurante");
+  },
+  registro_Restaurante: function (req, res) {
+    res.render("restaurantes/registro_Restaurante");
+  },
+  registro_Comercio: function (req, res) {
+    res.render("restaurantes/registro_Comercio");
+  },
+  repartidor: function (req, res) {
+    res.render("repartidor/repartidor");
+  },
+  registro_Repartidor: function (req, res) {
+    res.render("repartidor/registro_Repartidor");
+  },
+  login_Repartidor: function (req, res) {
+    res.render("repartidor/login_Repartidor");
+  },
+  about: function (req, res) {
+    res.render("nosotros/about");
+  },
+};
+
+module.exports = mainController;
