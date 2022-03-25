2b5d9c09ef7eebad93de62facdc618996993018b
Alejandra Nieto <malejis16@hotmail.com>
2022-03-24 19:00:29 -0500

    css

--- Rollereats_App/src/routes/mainRouter.js
+++ Rollereats_App/src/routes/mainRouter.js
@@ -8,24 +8,24 @@ const controller = require("../controllers/mainController");
 rutas.get("/", controller.index);
 
 //Rutas Users
-rutas.get("/login", controller.login);
-rutas.get("/register", controller.register);
+//rutas.get("/login", controller.login);
+//rutas.get("/register", controller.register);
 
 //Rutas Productos
-rutas.get("/carro", controller.carro);
-rutas.get("/creacionProducto", controller.creacionProducto);
+//rutas.get("/carro", controller.carro);
+//rutas.get("/creacionProducto", controller.creacionProducto);
 
 //Rutas Restaurantes
-rutas.get("/restaurante", controller.restaurante);
-rutas.use("/registro_Restaurante", controller.registro_Restaurante);
-rutas.use("/registro_Comercio", controller.registro_Comercio);
+//rutas.get("/restaurante", controller.restaurante);
+//rutas.use("/registro_Restaurante", controller.registro_Restaurante);
+//rutas.use("/registro_Comercio", controller.registro_Comercio);
 
 //Rutas Repartidor
-rutas.use("/repartidor", controller.repartidor);
-rutas.use("/registro_Repartidor", controller.registro_Repartidor);
-rutas.use("/login_Repartidor", controller.login_Repartidor);
+//rutas.use("/repartidor", controller.repartidor);
+//rutas.use("/registro_Repartidor", controller.registro_Repartidor);
+//rutas.use("/login_Repartidor", controller.login_Repartidor);
 
 //Rutas Nosotros
-rutas.use("/about", controller.about);
+//rutas.use("/about", controller.about);
 
 module.exports = rutas;
