6287f11eab622776b70c9cfb1d4fb8f9a0557dbd
Cristian-Giraldo <naitsircarevir@gmail.com>
2022-05-23 19:27:33 -0500

    Restablecimiento de la rama

--- Rollereats_App/src/middlewares/guestMiddleware.js
+++
@@ -1,9 +0,0 @@
-function guestMiddleware(req, res, next) {
-  if (req.session.usuarioLogueado == undefined) {
-    next();
-  } else {
-    res.redirect("/usuarios/login");
-  }
-}
-
-module.exports = guestMiddleware;
--- Rollereats_App/src/middlewares/loginMiddleware.js
+++
@@ -1,9 +0,0 @@
-function loginMiddleware(req, res, next) {
-  if (req.session.usuarioLogueado != undefined) {
-    next();
-  } else {
-    res.redirect("/usuarios/login");
-  }
-}
-
-module.exports = loginMiddleware;
